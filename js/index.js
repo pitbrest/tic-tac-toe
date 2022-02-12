161cbd9387ccc2880b7824008f97b4144af34454
pitbrest <sasha.petrov@tut.by>
2022-02-12 15:46:17 +0300

    some commit

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@ let move = 0
 let winner = ''
 
 
-area.addEventListener ('click', event => {
+area.addEventListener('click', event => {
 
-	if(event.target.className === 'box') {
-		move % 2 === 0 && event.target.innerHTML === '' ? event.target.innerHTML = '<img src="svg/cross.svg" src="img" width="40" height="40">' : 
-		event.target.innerHTML = '<img src="svg/circle.svg" src="img" width="40" height="40">';
+	if (event.target.className === 'box') {
+		move % 2 === 0 && event.target.innerHTML === '' ? event.target.innerHTML = '<img src="svg/cross.svg" src="img" width="40" height="40">' :
+			event.target.innerHTML = '<img src="svg/circle.svg" src="img" width="40" height="40">';
 		move += 1;
-	}		
-	findWinner()
+		setTimeout(() => {
+			findWinner()
+		}, 100);
+	}
+	
 })
 
 
@@ -30,16 +33,19 @@ function findWinner() {
 		[2, 4, 6],
 	];
 
-	for(let i=0; i<winArr.length; i++) {
-		if(boxes[winArr[i][0]].innerHTML.includes('cross') && boxes[winArr[i][1]].innerHTML.includes('cross') && boxes[winArr[i][2]].innerHTML.includes('cross')) {
+	for (let i = 0; i < winArr.length; i++) {
+		if (boxes[winArr[i][0]].innerHTML.includes('cross') && boxes[winArr[i][1]].innerHTML.includes('cross') && boxes[winArr[i][2]].innerHTML.includes('cross')) {
 			winner = 'крестики'
 			alert('Победили крестики !!!')
+			boxes.forEach(item => item.innerHTML = '')
 		}
-		if(boxes[winArr[i][0]].innerHTML.includes('circle') && boxes[winArr[i][1]].innerHTML.includes('circle') && boxes[winArr[i][2]].innerHTML.includes('circle')) {
+		if (boxes[winArr[i][0]].innerHTML.includes('circle') && boxes[winArr[i][1]].innerHTML.includes('circle') && boxes[winArr[i][2]].innerHTML.includes('circle')) {
 			winner = 'нолики'
 			alert('Победили нолики !!!')
+			boxes.forEach(item => item.innerHTML = '')
 		}
 	}
+	
 }
 
 
