5cdc82fba02a3e51ebd5a1f32c49290543565480
pitbrest <sasha.petrov@tut.by>
2022-02-13 00:13:39 +0300

    some commit

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ let winner = ''
 
 
 area.addEventListener('click', event => {
-	
+
 	if (event.target.className === 'box') {
 		move % 2 === 0 && event.target.innerHTML === '' ? event.target.innerHTML = '<img src="assets/svg/cross.svg" alt="img" width="40" height="40">' : event.target.innerHTML = '<img src="assets/svg/circle.svg" alt="img" width="40" height="40">';
 		move += 1;
@@ -35,14 +35,14 @@ function findWinner() {
 
 	for (let i = 0; i < winArr.length; i++) {
 		if (boxes[winArr[i][0]].innerHTML.includes('cross') && boxes[winArr[i][1]].innerHTML.includes('cross') && boxes[winArr[i][2]].innerHTML.includes('cross')) {
-			winner = 'крестики';
 			sound(trackTwo)
+			winner = 'крестики';
 			alert('Победили крестики !!!');
 			clearResults();
 		}
 		if (boxes[winArr[i][0]].innerHTML.includes('circle') && boxes[winArr[i][1]].innerHTML.includes('circle') && boxes[winArr[i][2]].innerHTML.includes('circle')) {
-			winner = 'нолики';
 			sound(trackTwo)
+			winner = 'крестики';
 			alert('Победили нолики !!!');
 			clearResults();
 		}
@@ -78,7 +78,7 @@ function sound(track) {
 // Озвучка если пользователь кликает мимо игрового поля
 
 body.addEventListener('click', event => {
-	if(event.target.className != 'box') {
+	if (event.target.className != 'box') {
 		sound(trackOne)
 	}
 })
